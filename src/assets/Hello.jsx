0c118d4b8244144c63d2e600f7a6b271e7cf446b
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-02-26 10:00:35 +0700

    react

---
+++ src/assets/Hello.jsx
@@ -0,0 +1,17 @@
+const Hello = ({message,name}) => {
+    console.log({message,name})
+    return(
+        <div>
+            <h1>
+                {name} {message} 
+            </h1>
+        </div>
+    );
+};
+
+import PropTypes from "prop-types";
+Hello.PropTypes={
+    message:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired
+}
+export default Hello;
